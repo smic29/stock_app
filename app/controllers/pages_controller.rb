416519afbbd56272057c9b2c3315ca3765ff63b6
smic29 <sibulo1991@gmail.com>
2024-04-07 10:55:54 +0800

    Cleaned up routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,5 @@
 class PagesController < ApplicationController
   def home
-    redirect_to admin_path if current_user&.admin
+    redirect_to admin_index_path if current_user&.admin
   end
-
-  def admin
-    redirect_to root_path if !current_user&.admin
-  end
-
 end
