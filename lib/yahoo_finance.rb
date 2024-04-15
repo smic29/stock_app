f4d45b7a5af4d1bcd9291aaae00ae5604233da2b
smic29 <sibulo1991@gmail.com>
2024-04-15 17:41:35 +0800

    Adjusted code to allow for multiple query requests, modified has returned

--- lib/yahoo_finance.rb
+++ lib/yahoo_finance.rb
@@ -13,31 +13,32 @@ module YahooFinance
 
     def quote(symbol)
       hash_result = {}
+      symbols = convert_to_arr(symbol)
 
-      #Experienced some symbols returning empty values. I'm not sure what's causing that.
+      # Experienced some symbols returning empty values. I'm not sure what's causing that.
       et_time_now = Time.now.in_time_zone('Eastern Time (US & Canada)')
       start_time = et_time_now - 7.days
       end_ts = et_time_now.to_i
       start_ts = start_time.to_i
 
-      uri = URI("#{API_URL}/v7/finance/chart/#{symbol}?period1=#{start_ts}&period2=#{end_ts}&interval=1d&events=history&includeAdjustedClose=true")
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.read_timeout = 10
+      symbols.each do |sym|
+        uri = URI("#{API_URL}/v7/finance/chart/#{sym}?period1=#{start_ts}&period2=#{end_ts}&interval=1d&events=history&includeAdjustedClose=true")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.read_timeout = 10
 
-      # puts uri -> was testing failed queries, kept it here as placeholder for when I want
-      # to write code on making symbols into an array. Maybe. who knows.
+        request = Net::HTTP::Get.new(uri)
+        request['User-Agent'] = "SpicyStockApp/1.0"
 
-      request = Net::HTTP::Get.new(uri)
-      request['User-Agent'] = "SpicyStockApp/1.0"
+        response = http.request(request)
 
-      response = http.request(request)
-
-      if response.is_a?(Net::HTTPSuccess)
-        hash_result.store(symbol, process_output(JSON.parse(response.body)))
-      else
-        puts "Failed to fetch data for symbol #{symbol}. HTTP Status: #{response.code}"
-        hash_result.store(symbol, "No data found")
+        if response.is_a?(Net::HTTPSuccess)
+          hash_result.store(sym, process_output(JSON.parse(response.body)))
+          puts uri
+        else
+          puts "Failed to fetch data for symbol #{sym}. HTTP Status: #{response.code}"
+          hash_result.store(sym, "No data found")
+        end
       end
 
       hash_result
@@ -47,6 +48,7 @@ module YahooFinance
     private
 
     def process_output(json)
+      result = {}
       base = json["chart"]["result"]
       return "No data available" if base == nil
 
@@ -54,7 +56,12 @@ module YahooFinance
 
       return "Price history unavailable" if price == nil
 
-      price.sample.round(2)
+      result["price"] = price.sample.round(2)
+      result
+    end
+
+    def convert_to_arr(symbol)
+      symbol.instance_of?(Array) ? symbol : [symbol]
     end
   end
 end
