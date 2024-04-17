b4f8e76bb45d1561503baed005e7fbbc81e25dc4
smic29 <sibulo1991@gmail.com>
2024-04-17 19:43:34 +0800

    Added response logic to transaction create to update displayed form

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,31 +7,35 @@ class TransactionsController < ApplicationController
     # price = params[:price].to_f
     # total = params[:quantity].to_f * price
 
-    if params[:commit] == 'Buy'
+    respond_to do |format|
+      if params[:commit] == 'Buy'
+        ActiveRecord::Base.transaction do
+          # Params required for transaction creation
+          create_params = transaction_params
+          create_params[:type] = params[:commit]
+          @stock = current_user.stocks.find_or_create_by(symbol: create_params[:symbol].upcase)
+          create_params[:stock_id] = @stock.id
+          create_params = create_params.except(:symbol) # Needed to remove since it's not part of the transactions table
+          price = create_params[:price].to_f
+          total = create_params[:quantity].to_f * price
+          cash = current_user.cash
 
-      ActiveRecord::Base.transaction do
-        # Params required for transaction creation
-        create_params = transaction_params
-        create_params[:type] = params[:commit]
-        @stock = current_user.stocks.find_or_create_by(symbol: create_params[:symbol].upcase)
-        create_params[:stock_id] = @stock.id
-        create_params = create_params.except(:symbol) # Needed to remove since it's not part of the transactions table
-        price = create_params[:price].to_f
-        total = create_params[:quantity].to_f * price
-        cash = current_user.cash
+          # Creating the transaction
+          @transaction = current_user.transactions.new(create_params)
+          @transaction.save!
 
-        # Creating the transaction
-        @transaction = current_user.transactions.new(create_params)
-        @transaction.save!
+          # Updating the stock
+          @stock.increment!(:quantity, create_params[:quantity].to_i)
 
-        # Updating the stock
-        @stock.increment!(:quantity, create_params[:quantity].to_i)
+          # Updating current cash
+          current_user.update!(cash: cash - total)
 
-        # Updating current cash
-        current_user.update!(cash: cash - total)
+          format.turbo_stream { render turbo_stream: [
+            turbo_stream.update("transaction-form", partial: "pages/transact_form", locals: { key: @stock.symbol, price: price})
+            ]
+          }
+        end
       end
-
-
     end
   end
 
