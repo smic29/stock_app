20ba997cd514267c367ca8500d46c8099df635c0
smic29 <sibulo1991@gmail.com>
2024-04-19 23:37:13 +0800

    Added validations and scopes to stock model

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,6 +2,6 @@ class StocksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @stocks = current_user.stocks.where("quantity > ?", 0 )
+    @stocks = current_user.stocks.displayable
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,16 @@
 class Stock < ApplicationRecord
   belongs_to :user
   has_many :transactions
+
+  validates :symbol, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validate :user_approved
+
+  scope :displayable, -> { joins(:user).merge(User.is_verified_trader).where("quantity > ?", 0 ) }
+
+  private
+
+  def user_approved
+    errors.add(:user, "must be approved") unless user&.approved?
+  end
 end
