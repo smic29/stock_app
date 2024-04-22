ba02473fe586191d083d3b3cc3b4dfc354f07520
smic29 <sibulo1991@gmail.com>
2024-04-22 10:30:22 +0800

    Added concern tests on stock model spec

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -4,10 +4,37 @@ RSpec.describe Stock, type: :model do
   let(:user) { create(:user, approved: true) }
   let(:unapproved_user) { create(:user, approved: false) }
 
+  describe 'Concerns' do
+    let(:stock) { build(:stock, user: user, quantity: 1) }
+    let(:invalid_stock) { build(:stock, user: unapproved_user, quantity: 1) }
+
+    context 'when the user is approved' do
+      it 'is valid' do
+        expect(stock).to be_valid
+      end
+    end
+
+    context 'when the user is not approved' do
+      it 'is not valid' do
+        expect(invalid_stock).not_to be_valid
+        expect(invalid_stock.errors[:user]).to include("Trades have not been approved yet")
+      end
+    end
+
+    context 'when no user is provided' do
+      let(:stock) { build(:stock, quantity: 1, user: nil) }
+
+      it 'is not valid' do
+        expect(stock).not_to be_valid
+        expect(stock.errors[:user]).to include("must exist")
+      end
+    end
+  end
+
   describe "Validations" do
     it { should validate_presence_of(:symbol) }
     it { should validate_numericality_of(:quantity).is_greater_than_or_equal_to(0) }
-    it { should_not allow_value(nil).for(:user).with_message("Trades have not been approved yet") }
+    # it { should_not allow_value(nil).for(:user).with_message("Trades have not been approved yet") }
     puts "Stock: Validations Passed"
   end
 
@@ -35,4 +62,5 @@ RSpec.describe Stock, type: :model do
     end
     puts "Stock: Scopes Passed"
   end
+
 end
