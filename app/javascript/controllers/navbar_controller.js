0b95dcfd5471b6a5de94fa0090863ec2af919714
smic29 <sibulo1991@gmail.com>
2024-04-22 21:47:47 +0800

    Added a stimulus controller to navbar

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  connect() {
+    document.addEventListener('turbo:before-frame-render',(e) => {
+      const turboFrame = e.target
+
+      if (turboFrame.id !== "user_frame") {
+        return
+      }
+      const links = this.element.querySelectorAll('a')
+
+      links.forEach((link) => {
+        if (link.href !== turboFrame.src) {
+          link.classList.remove('active')
+        } else {
+          link.classList.add('active')
+        }
+      })
+    })
+  }
+}
