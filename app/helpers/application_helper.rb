90edf0fa9696ac70af60e455cff7053a16b2e103
smic29 <sibulo1991@gmail.com>
2024-04-23 13:56:19 +0800

    Refactored links from navbar partial to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,21 @@ module ApplicationHelper
   def pending_users?
     User.is_pending_approval.exists?
   end
+
+  def pending_users_link
+    link_to pending_approval_path, data: { turbo_frame: 'admin_dash' },
+      class: "dash-link #{pending_users? ? '' : 'disabled' }" do
+        if pending_users?
+          content_tag(:i, '', class: 'fa-solid fa-person-circle-exclamation fa-xl')
+        else
+          content_tag(:i, '', class: 'fa-solid fa-person fa-xl')
+        end
+      end
+  end
+
+  def nav_link(path, frame, icon, active = false)
+    link_to path, data: { turbo_frame: frame }, class: "dash-link #{active ? 'active' : ''}" do
+      content_tag(:i, '', class: "fa-solid fa-xl #{icon}")
+    end
+  end
 end
