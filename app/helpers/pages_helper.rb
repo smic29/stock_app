906ef83f5cbe36da722b228638a78e0368a935bc
smic29 <sibulo1991@gmail.com>
2024-04-24 23:46:20 +0800

    Added a progress bar and conditionals for portfolio values

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,22 +1,44 @@
 module PagesHelper
+  def get_market_value
+    @market_value ||= begin
+      total_value = 0
+      stock_data = session[:symbol_data]
 
+      stock_data.each do |symbol, data|
+        average_price = data["prices"].sum / data["prices"].size.to_f
+        user_stock = current_user.stocks.find_by(symbol: symbol)
 
-def get_portfolio_value
-  return if session[:symbol_data].nil?
+        if user_stock
+          total_value += average_price * user_stock.quantity
+        end
+      end
 
-  total_value = 0
-  stock_data = session[:symbol_data]
-
-  stock_data.each do |symbol, data|
-    average_price = data["prices"].sum / data["prices"].size.to_f
-    user_stock = current_user.stocks.find_by(symbol: symbol)
-
-    if user_stock
-      total_value += average_price * user_stock.quantity
+      total_value
     end
   end
 
-  total_value / current_user.stocks.not_zero.count
-end
+  def get_transaction_value
+    @transaction_value ||= begin
+      buy_transactions = current_user.transactions.where(type: 'Buy').sum(:price)
+      sell_transactions = current_user.transactions.where(type: 'Sell').sum(:price)
 
+      net_transaction_value = buy_transactions - sell_transactions
+    end
+  end
+
+  def portfolio_performance_percentage
+    @percentage_value ||= begin
+      transaction_value = get_transaction_value
+
+      market_value = get_market_value
+
+      percentage_difference = ((market_value - transaction_value) / transaction_value) * 100
+
+      percentage_difference
+    end
+  end
+
+  def is_gain?
+    portfolio_performance_percentage >= 0
+  end
 end
