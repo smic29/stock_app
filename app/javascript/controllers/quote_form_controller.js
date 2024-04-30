80edfb43d5c88caf17ae666d4a4729a719d40e6e
smic29 <sibulo1991@gmail.com>
2024-04-30 13:15:12 +0800

    Added client-side input validations

--- app/javascript/controllers/quote_form_controller.js
+++ app/javascript/controllers/quote_form_controller.js
@@ -7,16 +7,28 @@ export default class extends Controller {
     const inputField = this.element.querySelector('#symbol')
     const options = this.element.querySelectorAll('.dropdown-item')
     const frame = document.querySelector('#quote_result_frame')
+    const submitButton = this.element.querySelector('button.custom-btn')
 
     btn.addEventListener('click', () => {
       inputField.value = ''
+      submitButton.disabled = true
+      inputField.focus()
     })
 
     options.forEach(item => {
       item.addEventListener('click',() => {
         const symbol = item.textContent.trim()
         inputField.value = symbol
+        submitButton.disabled = false
+        inputField.focus()
       })
     })
+  }
+
+  toggleSubmitButton(e) {
+    const submitButton = this.element.querySelector('button.custom-btn')
+
+    console.log(e.target.value.trim() === '')
+    submitButton.disabled = e.target.value.trim() === "";
   }
 }
