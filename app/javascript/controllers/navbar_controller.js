461bee1e5f29dede414eb70242e49d1d6a6f7645
smic29 <sibulo1991@gmail.com>
2024-05-01 21:08:24 +0800

    Updated navbar active status checks

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,21 +3,51 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
-    document.addEventListener('turbo:before-frame-render',(e) => {
-      const turboFrame = e.target
-
-      if (turboFrame.id !== "user_frame" && turboFrame.id !== "admin_dash") {
-        return
+    const frame = document.querySelector('turbo-frame')
+    
+    if (frame.id === 'user_frame' || frame.id === 'admin_dash'){
+      if (frame.src=== null) {
+        frame.src = window.location.href
+        this.setActive(frame)
       }
-      const links = this.element.querySelectorAll('a')
+    }
 
-      links.forEach((link) => {
-        if (link.href !== turboFrame.src) {
-          link.classList.remove('active')
-        } else {
-          link.classList.add('active')
-        }
-      })
+
+
+    document.addEventListener('turbo:before-frame-render',(e) => {
+      this.isItemActive(e)
+    })
+  }
+
+  isItemActive(e) {
+    const turboFrame = e.target
+
+    if (turboFrame.id !== "user_frame" && turboFrame.id !== "admin_dash") {
+      return
+    }
+    const links = this.element.querySelectorAll('a')
+
+    links.forEach((link) => {
+      if (link.href !== turboFrame.src) {
+        link.classList.remove('active','pe-none')
+      } else {
+        link.classList.add('active','pe-none')
+      }
+    })
+  }
+
+  setActive(turboFrame) {
+    if (turboFrame.id !== "user_frame" && turboFrame.id !== "admin_dash") {
+      return
+    }
+    const links = this.element.querySelectorAll('a')
+
+    links.forEach((link) => {
+      if (link.href !== turboFrame.src) {
+        link.classList.remove('active','pe-none')
+      } else {
+        link.classList.add('active','pe-none')
+      }
     })
   }
 }
