ec6c219301db2cf3541c00dded5f84368412d108
smic29 <sibulo1991@gmail.com>
2024-05-05 20:09:56 +0800

    Added default for host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
+  config.action_mailer.default_url_options = { host: 'stockapp-production.up.railway.app' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
