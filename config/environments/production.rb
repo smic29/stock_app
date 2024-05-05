56b4ca9a8603431a2de8b785faad303b1d5ba583
smic29 <sibulo1991@gmail.com>
2024-05-05 20:52:52 +0800

    Updated smtp used

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.mail.yahoo.com',
+    address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['EMAIL_ADDRESS'],
     password: ENV['EMAIL_PASSWORD'],
